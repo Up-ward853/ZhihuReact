11e7b5f0494638d1a7c872e0588bf33e1f3dec44
Up-ward853 <56709348+Up-ward853@users.noreply.github.com>
2019-10-21 10:23:05 +0800

    Add files via upload

---
+++ precache-manifest.b9876b38fc5963cd71a1052055dcbc57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fee8526be603c038b564d7ba54b33e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "157ff6efbd3df60119da",
+    "url": "/static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "06738ebfadfea4800ea5",
+    "url": "/static/css/main.445a3821.chunk.css"
+  },
+  {
+    "revision": "157ff6efbd3df60119da",
+    "url": "/static/js/2.1655f2af.chunk.js"
+  },
+  {
+    "revision": "06738ebfadfea4800ea5",
+    "url": "/static/js/main.f1726ea2.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  },
+  {
+    "revision": "bad4d15aa26f5e0197adf01b3e5e2192",
+    "url": "/static/media/head.bad4d15a.png"
+  }
+]);
